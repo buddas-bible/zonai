6312e639ff67d76fa2f9821fdedfdd6d0bc95feb
buddas-bible <kh951220@gmail.com>
2026-09-15 01:45:32 +0900

    su
    나누기 연산을 곱연산으로 수정

--- src/math/vec2.h
+++ src/math/vec2.h
@@ -1,97 +1,105 @@
 #pragma once
+#include <cmath>
 
 namespace zonai
 {
-    struct Vec2
+    struct vec2
     {
         float x = 0.0f;
         float y = 0.0f;
 
-        Vec2& operator+=(const Vec2& rhs)
+        vec2& operator+=(const vec2& rhs)
         {
             x += rhs.x;
             y += rhs.y;
             return *this;
         }
 
-        Vec2& operator-=(const Vec2& rhs)
+        vec2& operator-=(const vec2& rhs)
         {
             x -= rhs.x;
             y -= rhs.y;
             return *this;
         }
 
-        Vec2& operator*=(float scalar)
+        vec2& operator*=(float scalar)
         {
             x *= scalar;
             y *= scalar;
             return *this;
         }
 
-        Vec2& operator/=(float scalar)
+        vec2& operator/=(float scalar)
         {
-            x /= scalar;
-            y /= scalar;
+            assert(scalar != 0.0f);
+
+			float temp = 1.0f / scalar;
+
+            x *= temp;
+            y *= temp;
             return *this;
         }
     };
 
-    inline Vec2 operator+(Vec2 lhs, const Vec2& rhs)
+    inline vec2 operator+(vec2 lhs, const vec2& rhs)
     {
         lhs += rhs;
         return lhs;
     }
 
-    inline Vec2 operator-(Vec2 lhs, const Vec2& rhs)
+    inline vec2 operator-(vec2 lhs, const vec2& rhs)
     {
         lhs -= rhs;
         return lhs;
     }
 
-    inline Vec2 operator-(const Vec2& value)
+    inline vec2 operator-(const vec2& value)
     {
         return { -value.x, -value.y };
     }
 
-    inline Vec2 operator*(Vec2 value, float scalar)
+    inline vec2 operator*(vec2 value, float scalar)
     {
         value *= scalar;
         return value;
     }
 
-    inline Vec2 operator*(float scalar, Vec2 value)
+    inline vec2 operator*(float scalar, vec2 value)
     {
         value *= scalar;
         return value;
     }
 
-    inline Vec2 operator/(Vec2 value, float scalar)
+    inline vec2 operator/(vec2 value, float scalar)
     {
-        value /= scalar;
+        assert(scalar != 0.0f);
+
+		float temp = 1.0f / scalar;
+        value *= temp;
         return value;
     }
 
-    inline float Dot(const Vec2& a, const Vec2& b)
+    inline float Dot(const vec2& a, const vec2& b)
     {
         return a.x * b.x + a.y * b.y;
     }
 
-    inline float Cross(const Vec2& a, const Vec2& b)
+    inline float Cross(const vec2& a, const vec2& b)
     {
         return a.x * b.y - a.y * b.x;
     }
 
-    inline float LengthSquared(const Vec2& value)
+    inline float LengthSquared(const vec2& value)
     {
         return Dot(value, value);
     }
 
-    inline float Length(const Vec2& value)
+    inline float Length(const vec2& value)
     {
         return std::sqrt(LengthSquared(value));
     }
 
-    inline Vec2 Normalize(const Vec2& value)
+    inline vec2 Normalize(const vec2& value)
     {
         const float length = Length(value);
 
