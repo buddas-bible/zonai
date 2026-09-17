e37a0cbd0c8cdc62d13311dc71aa5d7392aea5f6
buddas-bible <104293172+buddas-bible@users.noreply.github.com>
2026-09-18 01:41:20 +0900

    refactor: organize narrowphase collision families

--- src/collision/narrowphase/collidePolygons.cpp
+++ src/collision/narrowphase/collidePolygons.cpp
@@ -485,4 +485,21 @@ localManifold2 CollidePolygons(
     return manifold;
 }
 
+localManifold2 CollidePolygonCapsule(
+    const polygon2& polygon,
+    const capsule2& capsule, const transform2& capsuleTransform )
+{
+    const polygon2 capsulePolygon = MakeCapsule(
+        capsule.center1,
+        capsule.center2,
+        capsule.radius
+    );
+
+    return CollidePolygons(
+        polygon,
+        capsulePolygon,
+        capsuleTransform
+    );
+}
+
 } // namespace zonai
