747e3337b99af31ed9cf41b81a2b56b74ce732c1
buddas-bible <104293172+buddas-bible@users.noreply.github.com>
2026-09-22 01:55:40 +0900

    test: define broad phase proxy key

--- tests/collision/broadphase/broad_phase_test.cpp
+++ tests/collision/broadphase/broad_phase_test.cpp
@@ -7,6 +7,16 @@ using namespace zonai;
 
 int main()
 {
+    static_assert( MakeProxyKey( 0, BodyType::Static ) == 0 );
+    static_assert( MakeProxyKey( 0, BodyType::Kinematic ) == 1 );
+    static_assert( MakeProxyKey( 0, BodyType::Dynamic ) == 2 );
+    static_assert( MakeProxyKey( 7, BodyType::Dynamic ) == 30 );
+
+    constexpr ProxyKey key = MakeProxyKey( 17, BodyType::Kinematic );
+
+    static_assert( GetProxyId( key ) == 17 );
+    static_assert( GetProxyType( key ) == BodyType::Kinematic );
+
     BroadPhase broadPhase{};
 
     DynamicTree& staticTree = broadPhase.GetTree( BodyType::Static );
